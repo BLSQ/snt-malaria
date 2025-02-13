55e038275c536ac44fba648c3d8306d7f5ea5c5e
Christophe Gerard <cgerard@bluesquarehub.com>
2025-02-13 15:24:36 +0100

    display a map of org units

---
+++ js/src/domains/planning/hooks/useGetOrgUnits.ts
@@ -0,0 +1,22 @@
+import { UseQueryResult } from 'react-query';
+import { OrgUnit } from 'Iaso/domains/orgUnits/types/orgUnit';
+import { getRequest } from 'Iaso/libs/Api';
+import { useSnackQuery } from 'Iaso/libs/apiHooks';
+import { makeUrlWithParams } from 'Iaso/libs/utils';
+
+export const useGetOrgUnits = (): UseQueryResult<OrgUnit[], Error> => {
+    const params: Record<string, any> = {
+        validation_status: 'VALID',
+        asLocation: true,
+        limit: 8000,
+    };
+    const url = makeUrlWithParams('/api/orgunits/', params);
+    return useSnackQuery({
+        queryKey: ['orgUnits', params],
+        queryFn: () => getRequest(url),
+        options: {
+            staleTime: 1000 * 60 * 15, // in MS
+            cacheTime: 1000 * 60 * 5,
+        },
+    });
+};
