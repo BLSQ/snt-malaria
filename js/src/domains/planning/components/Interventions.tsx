ff37d0cceef9714fd36a87d7e86fdcb6f491dcc9
Bram Jans <ik@bramjans.com>
2025-02-17 10:09:45 +0100

    SNT-15 Re-organize accordions for different sections

--- js/src/domains/planning/components/Interventions.tsx
+++ js/src/domains/planning/components/Interventions.tsx
@@ -1,12 +1,28 @@
 import React, { FC } from 'react';
-import { Box, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {
+    Box,
+    Typography,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+} from '@mui/material';
 
 export const Interventions: FC = () => {
     return (
-        <Box p={2}>
-            <Typography variant="h6" gutterBottom color="secondary">
-                INTERVENTIONS
-            </Typography>
-        </Box>
+        <Accordion>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="interventions-content"
+                id="interventions-header"
+            >
+                <Typography variant="h6" gutterBottom color="secondary">
+                    Intervention mix
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Box p={2}>{/* Add content for intervention mix here */}</Box>
+            </AccordionDetails>
+        </Accordion>
     );
 };
