276eb9ceb0e8b9833db0a6aa3d286629401f77df
HAKIZIMANA Franck <fhakizimana@bluesquarehub.com>
2025-02-25 10:48:48 +0200

    Not use modal for now

--- js/src/domains/planning/components/interventionMix/InterventionCategories.tsx
+++ js/src/domains/planning/components/interventionMix/InterventionCategories.tsx
@@ -1,9 +1,12 @@
-import React, { FC, useCallback, useState } from 'react';
-import { Grid, Typography } from '@mui/material';
+import React, { FC, useCallback, useMemo, useState } from 'react';
+import { Button, Divider, Grid, Typography } from '@mui/material';
+import { useSafeIntl } from 'bluesquare-components';
 import { useGetInterventionCategories } from '../../hooks/useGetInterventions';
+import { MESSAGES } from '../../messages';
 import { Interventions } from './Interventions';
 
 export const InterventionCategories: FC = () => {
+    const { formatMessage } = useSafeIntl();
     const [selectedInterventions, setSelectedInterventions] = useState<{
         [categoryId: number]: number | null;
     }>({});
@@ -23,39 +26,78 @@ export const InterventionCategories: FC = () => {
         },
         [],
     );
-    return (
-        <Grid container direction="row" spacing={2} padding={2}>
-            {!isLoadingInterventionCategories &&
-                interventionCategories?.map(interventionCategory => {
-                    return (
-                        <Grid item key={interventionCategory.id}>
-                            <Grid item>
-                                <Typography
-                                    sx={{
-                                        fontSize: '0.75rem',
-                                    }}
-                                >
-                                    {interventionCategory.name}
-                                </Typography>
-                            </Grid>
 
-                            <Interventions
-                                interventionCategoryId={interventionCategory.id}
-                                interventions={
-                                    interventionCategory.interventions
-                                }
-                                selectedId={
-                                    selectedInterventions[
+    const selectedInterventionValues = useMemo(
+        () =>
+            Object.values(selectedInterventions).filter(
+                value => value !== null,
+            ),
+        [selectedInterventions],
+    );
+
+    return (
+        <>
+            <Grid container direction="row" spacing={2} padding={2}>
+                {!isLoadingInterventionCategories &&
+                    interventionCategories?.map(interventionCategory => {
+                        return (
+                            <Grid item key={interventionCategory.id}>
+                                <Grid item>
+                                    <Typography
+                                        sx={{
+                                            fontSize: '0.75rem',
+                                        }}
+                                    >
+                                        {interventionCategory.name}
+                                    </Typography>
+                                </Grid>
+
+                                <Interventions
+                                    interventionCategoryId={
                                         interventionCategory.id
-                                    ] ?? null
-                                }
-                                handleSelectIntervention={
-                                    handleSelectIntervention
-                                }
-                            />
-                        </Grid>
-                    );
-                })}
-        </Grid>
+                                    }
+                                    interventions={
+                                        interventionCategory.interventions
+                                    }
+                                    selectedId={
+                                        selectedInterventions[
+                                            interventionCategory.id
+                                        ] ?? null
+                                    }
+                                    handleSelectIntervention={
+                                        handleSelectIntervention
+                                    }
+                                />
+                            </Grid>
+                        );
+                    })}
+            </Grid>
+            <Divider sx={{ width: '100%' }} />
+            <Grid
+                item
+                display="flex"
+                justifyContent="flex-end"
+                alignItems="flex-end"
+                padding={2}
+                sx={{
+                    height: '68px',
+                }}
+            >
+                <Button
+                    onClick={() => {
+                        console.log('hello');
+                    }}
+                    variant="contained"
+                    color="primary"
+                    sx={{
+                        fontSize: '0.875rem',
+                        textTransform: 'none',
+                    }}
+                    disabled={selectedInterventionValues.length === 0}
+                >
+                    {formatMessage(MESSAGES.applyMixAndAddPlan)}
+                </Button>
+            </Grid>
+        </>
     );
 };
--- js/src/domains/planning/components/interventionMix/InterventionsMix.tsx
+++ js/src/domains/planning/components/interventionMix/InterventionsMix.tsx
@@ -1,20 +1,29 @@
 import React, { FC } from 'react';
 
-import { Accordion, AccordionDetails, Divider } from '@mui/material';
+import {
+    Accordion,
+    AccordionDetails,
+    // Button,
+    Divider,
+    // Grid,
+} from '@mui/material';
 
-import { ApplyInterventionsMix } from './ApplyInterventionMixModal';
+// import { useSafeIntl } from 'bluesquare-components';
+// import { MESSAGES } from '../../messages';
+// import { ApplyInterventionsMix } from './ApplyInterventionMixModal';
 import { InterventionCategories } from './InterventionCategories';
 import { InterventionMixSummary } from './InterventionMixSummary';
 
 export const InterventionsMix: FC = () => {
+    // const { formatMessage } = useSafeIntl();
     return (
         <Accordion>
             <InterventionMixSummary />
             <AccordionDetails sx={{ padding: 0 }}>
                 <Divider sx={{ width: '100%' }} />
                 <InterventionCategories />
-                <Divider sx={{ width: '100%' }} />
-                <ApplyInterventionsMix iconProps={{}} />
+
+                {/* <ApplyInterventionsMix iconProps={{}} /> */}
             </AccordionDetails>
         </Accordion>
     );
