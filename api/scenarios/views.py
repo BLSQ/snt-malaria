013cf640d2f0566cc2d3eb673280e8bd45c16dfa
Bewi <benjamin.wilfart@gmail.com>
2025-06-10 11:59:34 +0200

    API: Duplicate scenario unique names & add swagger wrapper

--- api/scenarios/views.py
+++ api/scenarios/views.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+import re
 from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+
 
 from plugins.snt_malaria.models import InterventionAssignment, Scenario
 
@@ -9,6 +13,15 @@ from .serializers import DuplicateScenarioSerializer, ScenarioSerializer
 
 
 class ScenarioViewSet(viewsets.ModelViewSet):
+    """Scenario API
+
+    POST /api/scenarios/duplicate
+    Duplicate scenario for specified id
+    {
+        "id_to_duplicate": Int
+    }
+    """
+
     serializer_class = ScenarioSerializer
     ordering_fields = ["id", "name"]
     http_method_names = ["get", "post", "put"]
@@ -22,6 +35,7 @@ class ScenarioViewSet(viewsets.ModelViewSet):
         serializer.save()
 
     # Custom action to duplicate a scenario
+    @swagger_auto_schema(request_body=DuplicateScenarioSerializer(many=False))
     @action(detail=False, methods=["post"], url_path="duplicate")
     def duplicate(self, request):
         serializer = DuplicateScenarioSerializer(data=request.data, context={"request": request})
@@ -31,7 +45,12 @@ class ScenarioViewSet(viewsets.ModelViewSet):
         id_to_duplicate = serializer.validated_data.get("id_to_duplicate")
         scenario = get_object_or_404(Scenario, pk=id_to_duplicate)
         scenario.pk = None
-        scenario.name = f"Copy of {scenario.name}"
+        
+        # Clean scenario name to keep it clear in case of multiple copies
+        clearedName = scenario.name.replace("Copy of", "").strip()
+        clearedName = re.sub(r"\s- (\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})\.(\d{6})", "", clearedName)
+        # Using datetime to create a unique name for the duplicated scenario
+        scenario.name = f"Copy of {clearedName} - {datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}"
         scenario.save()
 
         # Duplicate related assignments
