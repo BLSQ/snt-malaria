18c4351236a9856284ba29b7f459f9e1cdfb2355
Bewi <benjamin.wilfart@gmail.com>
2025-07-09 16:07:55 +0200

    Small lint fix

--- js/src/domains/planning/components/MapLegend.tsx
+++ js/src/domains/planning/components/MapLegend.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { Paper, Box, Theme, Typography } from '@mui/material';
+import { Paper, Box, Theme } from '@mui/material';
 import { SxStyles } from 'Iaso/types/general';
 import { MetricType } from '../types/metrics';
 import { LinearLegend } from './legend/LinearLegend';
@@ -36,16 +36,28 @@ export const MapLegend: FunctionComponent<Props> = ({ metric }) => {
     return (
         <Paper elevation={1} sx={styles.root}>
             <Box sx={styles.legendContainer}>
-                {metric.legend_type === 'linear' ? (
-                    <LinearLegend domainAndRange={metric.legend_config} />
-                ) : metric.legend_type === 'ordinal' ? (
-                    <OrdinalLegend domainAndRange={metric.legend_config} />
-                ) : (
-                    <ThresholdLegend
-                        threshold={metric.legend_config}
-                        unit={metric.unit_symbol}
-                    />
-                )}
+                {(() => {
+                    if (metric.legend_type === 'linear') {
+                        return (
+                            <LinearLegend
+                                domainAndRange={metric.legend_config}
+                            />
+                        );
+                    }
+                    if (metric.legend_type === 'ordinal') {
+                        return (
+                            <OrdinalLegend
+                                domainAndRange={metric.legend_config}
+                            />
+                        );
+                    }
+                    return (
+                        <ThresholdLegend
+                            threshold={metric.legend_config}
+                            unit={metric.unit_symbol}
+                        />
+                    );
+                })()}
             </Box>
         </Paper>
     );
