c1122458dbd84f400a5f0e31e06371475dba42f6
Bewi <benjamin.wilfart@gmail.com>
2025-08-05 15:20:02 +0200

    Add configuration to show / hide basic menu items

--- plugin_settings.py
+++ plugin_settings.py
@@ -1,2 +1,2 @@
 INSTALLED_APPS = ["plugins.snt_malaria"]
-CONSTANTS = {"LOGO_PATH": "", "APP_TITLE": "SNT Malaria"}
+CONSTANTS = {"LOGO_PATH": "", "APP_TITLE": "SNT Malaria", "HIDE_BASIC_NAV_ITEMS": "yes"}
