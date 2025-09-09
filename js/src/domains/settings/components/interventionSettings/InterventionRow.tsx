1463b901e229c1eff7cd9a1ab7f50a38b8dc5e9f
Bewi <benjamin.wilfart@gmail.com>
2025-09-09 11:07:09 +0200

    SNT-126: Move styles out of HTML

--- js/src/domains/settings/components/interventionSettings/InterventionRow.tsx
+++ js/src/domains/settings/components/interventionSettings/InterventionRow.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import { Box, Typography } from '@mui/material';
 import { IconButton } from 'bluesquare-components';
+import { SxStyles } from 'Iaso/types/general';
 import { Intervention } from '../../../planning/types/interventions';
 import { MESSAGES } from '../../messages';
 
@@ -10,39 +11,40 @@ type Props = {
     onEditInterventionCost: (intervention: Intervention) => void;
 };
 
+const styles: SxStyles = {
+    row: {
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr',
+        alignItems: 'center',
+        marginTop: 1,
+        marginBottom: 1,
+    },
+    actionWrapper: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    cost: {
+        textAlign: 'right',
+        fontWeight: 'bold',
+        marginRight: 2,
+    },
+};
+
 export const InterventionRow: React.FC<Props> = ({
     intervention,
     onEditInterventionCost,
 }) => (
-    <Box
-        key={intervention.id}
-        sx={{
-            display: 'grid',
-            gridTemplateColumns: '1fr 1fr',
-            alignItems: 'center',
-            marginTop: 1,
-            marginBottom: 1,
-        }}
-    >
+    <Box key={intervention.id} sx={styles.row}>
         <Typography variant="subtitle2" color="textSecondary">
             {intervention.name}
         </Typography>
-        <Box
-            sx={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-            }}
-        >
+        <Box sx={styles.actionWrapper}>
             <Box>
                 <Typography
                     variant="subtitle2"
                     color="textPrimary"
-                    sx={{
-                        textAlign: 'right',
-                        fontWeight: 'bold',
-                        marginRight: 2,
-                    }}
+                    sx={styles.cost}
                 >
                     ${intervention.cost_per_unit ?? 'N/A'}
                 </Typography>
