0f4f5ab2de411ed168327c787d2097b4116240ab
Bewi <benjamin.wilfart@gmail.com>
2025-09-11 15:46:20 +0200

    Translate all

--- js/src/domains/settings/messages.ts
+++ js/src/domains/settings/messages.ts
@@ -29,8 +29,32 @@ export const MESSAGES = defineMessages({
         id: 'iaso.snt_malaria.settings.intervention.save',
         defaultMessage: 'Save intervention',
     },
-    removeDetailedCost: {
-        id: 'iaso.snt_malaria.settings.intervention.removeDetaildCost',
-        defaultMessage: 'Remove detailed cost',
+    removeCostLine: {
+        id: 'iaso.snt_malaria.settings.intervention.removeCostLine',
+        defaultMessage: 'Remove cost',
+    },
+    addCostLine: {
+        id: 'iaso.snt_malaria.settings.intervention.addCostLine',
+        defaultMessage: 'Add cost',
+    },
+    totalCost: {
+        id: 'iaso.snt_malaria.settings.intervention.totalCost',
+        defaultMessage: 'Total cost',
+    },
+    detailedCosts: {
+        id: 'iaso.snt_malaria.settings.intervention.detailedCosts',
+        defaultMessage: 'Detailed costs',
+    },
+    detailedCostLabel: {
+        id: 'iaso.snt_malaria.settings.intervention.detailedCostLabel',
+        defaultMessage: 'Item',
+    },
+    detailedCostUnitLabel: {
+        id: 'iaso.snt_malaria.settings.intervention.detailedCostUnitLabel',
+        defaultMessage: 'Cost Unit',
+    },
+    detailedCostCategoryLabel: {
+        id: 'iaso.snt_malaria.settings.intervention.detailedCostCategoryLabel',
+        defaultMessage: 'Category',
     },
 });
