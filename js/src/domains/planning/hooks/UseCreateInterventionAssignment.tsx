6ede2183471c0dcb7c79c79a9b5dfec396f7c911
Bewi <benjamin.wilfart@gmail.com>
2025-09-17 14:26:22 +0200

    SNT-146: Display amount of districts added to plan

--- js/src/domains/planning/hooks/UseCreateInterventionAssignment.tsx
+++ js/src/domains/planning/hooks/UseCreateInterventionAssignment.tsx
@@ -1,4 +1,6 @@
 import { UseMutationResult } from 'react-query';
+import { openSnackBar } from 'Iaso/components/snackBars/EventDispatcher';
+import { succesfullSnackBar } from 'Iaso/constants/snackBars';
 import { postRequest } from 'Iaso/libs/Api';
 import { useSnackMutation } from 'Iaso/libs/apiHooks';
 import { InterventionAssignmentCreate } from '../types/interventions';
@@ -8,4 +10,10 @@ export const UseCreateInterventionAssignment = (): UseMutationResult =>
         mutationFn: (body: InterventionAssignmentCreate) =>
             postRequest(`/api/snt_malaria/interventionassignments/`, body),
         invalidateQueryKey: ['interventionAssignments', 'budgets'],
+        showSucessSnackBar: false,
+        options: {
+            onSuccess: (data, variables, context) => {
+                openSnackBar(succesfullSnackBar(data.message, data.message));
+            },
+        },
     });
