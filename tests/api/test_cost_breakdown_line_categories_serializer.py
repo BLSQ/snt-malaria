7d55e50d1eba62ab34e26854f323ec8d0248698b
Bewi <benjamin.wilfart@gmail.com>
2025-09-24 11:42:03 +0200

    Split tests between api and serializer

---
+++ tests/api/test_cost_breakdown_line_categories_serializer.py
@@ -0,0 +1,14 @@
+from iaso.models.base import Account
+from iaso.test import APITestCase
+from plugins.snt_malaria.api.cost_breakdown_line.serializers import InterventionCostBreakdownLineCategoriesSerializer
+
+
+class InterventionCostBreakdownLineSerializerTests(APITestCase):
+    def setUp(cls):
+        cls.account = Account.objects.create(name="Test Account")
+        cls.user = cls.create_user_with_profile(username="testuser", account=cls.account)
+
+    def test_to_representation(self):
+        serializer = InterventionCostBreakdownLineCategoriesSerializer()
+        data = serializer.to_representation(("Procurement", "Procurement"))
+        self.assertEqual(data, {"value": "Procurement", "label": "Procurement"})
