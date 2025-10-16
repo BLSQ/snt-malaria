19b8f6f2212a8e41d81272244017fc3b9103277d
Bewi <benjamin.wilfart@gmail.com>
2025-10-16 10:33:24 +0200

    Finetuning

--- js/src/domains/scenarios/hooks/useUpdateScenario.tsx
+++ js/src/domains/scenarios/hooks/useUpdateScenario.tsx
@@ -3,9 +3,17 @@ import { putRequest } from 'Iaso/libs/Api';
 import { useSnackMutation } from 'Iaso/libs/apiHooks';
 import { Scenario } from '../types';
 
-export const useUpdateScenario = (scenarioId): UseMutationResult =>
+export const useUpdateScenario = (
+    scenarioId: number,
+    onSuccess?: (data: any, variables: any, context: any) => void,
+    onError?: (error: any, variables: any, context: any) => void,
+): UseMutationResult =>
     useSnackMutation({
         mutationFn: (body: Scenario) =>
             putRequest(`/api/snt_malaria/scenarios/${scenarioId}/`, body),
         invalidateQueryKey: ['scenarios', 'scenario', scenarioId],
+        options: {
+            onSuccess,
+            onError,
+        },
     });
