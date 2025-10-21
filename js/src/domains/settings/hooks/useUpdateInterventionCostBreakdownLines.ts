2dd35dc6ff6a4ddd8b174bd44c0f17fc6673ed28
Bewi <benjamin.wilfart@gmail.com>
2025-10-21 15:18:04 +0200

    Refresh properly

--- js/src/domains/settings/hooks/useUpdateInterventionCostBreakdownLines.ts
+++ js/src/domains/settings/hooks/useUpdateInterventionCostBreakdownLines.ts
@@ -1,28 +1,39 @@
-import { UseMutationResult } from 'react-query';
+import { UseMutationResult, useQueryClient } from 'react-query';
 import { postRequest } from 'Iaso/libs/Api';
 import { useSnackMutation } from 'Iaso/libs/apiHooks';
 import { InterventionCostBreakdownLine } from '../types/InterventionCostBreakdownLine';
 
-export const useUpdateInterventionCostBreakdownLines = (): UseMutationResult =>
-    useSnackMutation({
-        mutationFn: (body: {
-            interventionId: number;
-            year: number;
-            costs: InterventionCostBreakdownLine[];
-        }) => {
-            return Promise.all([
-                postRequest(
-                    `/api/snt_malaria/intervention_cost_breakdown_lines/`,
-                    {
-                        intervention: body.interventionId,
-                        year: body.year,
-                        costs: body.costs,
-                    },
-                ),
-            ]);
-        },
-        invalidateQueryKey: [
-            'interventionCostBreakdownLines',
-            'interventionCategories',
-        ],
-    });
+export const useUpdateInterventionCostBreakdownLines =
+    (): UseMutationResult => {
+        const queryClient = useQueryClient();
+
+        return useSnackMutation({
+            mutationFn: (body: {
+                interventionId: number;
+                year: number;
+                costs: InterventionCostBreakdownLine[];
+            }) => {
+                return Promise.all([
+                    postRequest(
+                        `/api/snt_malaria/intervention_cost_breakdown_lines/`,
+                        {
+                            intervention: body.interventionId,
+                            year: body.year,
+                            costs: body.costs,
+                        },
+                    ),
+                ]);
+            },
+            options: {
+                onSuccess: (
+                    _data,
+                    variables: { interventionId: number; year: number },
+                ) => {
+                    queryClient.invalidateQueries(
+                        `interventionCostBreakdownLines_${variables.interventionId}_${variables.year}`,
+                    );
+                    queryClient.invalidateQueries('interventionCategories');
+                },
+            },
+        });
+    };
