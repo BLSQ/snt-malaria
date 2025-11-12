310e06cf2755551209fe637d7f01c522aae7f15d
Bewi <benjamin.wilfart@gmail.com>
2025-11-12 11:18:37 +0100

    Small cleanup

--- js/src/domains/planning/hooks/useGetLatestCalculatedBudget.ts
+++ js/src/domains/planning/hooks/useGetLatestCalculatedBudget.ts
@@ -4,7 +4,7 @@ import { useSnackQuery } from 'Iaso/libs/apiHooks';
 import { BudgetCalculationResponse } from '../types/budget';
 
 export const useGetLatestCalculatedBudget = (
-    scenarioId: number | undefined,
+    scenarioId?: number,
 ): UseQueryResult<BudgetCalculationResponse, Error> => {
     return useSnackQuery({
         queryKey: ['calculated_budget', `calculated_budget_${scenarioId}`],
