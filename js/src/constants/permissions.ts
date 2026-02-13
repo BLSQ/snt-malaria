beb2c0a8388c7f502eae44d04bdf129d5cc1dcc2
Thibault Dethier <tdethier@bluesquarehub.com>
2026-02-13 13:37:33 +0100

    add permission consts in front

---
+++ js/src/constants/permissions.ts
@@ -0,0 +1,10 @@
+const SCENARIO_BASIC_WRITE = 'iaso_snt_scenario_basic_write';
+const SCENARIO_FULL_WRITE = 'iaso_snt_scenario_full_write';
+const SETTINGS_READ = 'iaso_snt_settings_read';
+const SETTINGS_WRITE = 'iaso_snt_settings_write';
+export {
+    SCENARIO_BASIC_WRITE,
+    SCENARIO_FULL_WRITE,
+    SETTINGS_READ,
+    SETTINGS_WRITE,
+};
