fe3709ded29419f841a92d371e441c0ee0766dda
Bewi <benjamin.wilfart@gmail.com>
2026-03-20 11:13:10 +0100

    Restore with Proper casing

---
+++ js/src/domains/planningV2/components/scenarioRule/scenarioRuleForm/ScenarioRuleFormWrapper.tsx
@@ -0,0 +1,99 @@
+import React, { FC, useCallback, useMemo } from 'react';
+import { useSafeIntl } from 'bluesquare-components';
+import { CardStyled } from '../../../../../components/CardStyled';
+import { ExtendedFormikProvider } from '../../../../../hooks/useGetExtendedFormikContext';
+import { MESSAGES } from '../../../../messages';
+import { useCreateUpdateScenarioRule } from '../../../hooks/useCreateUpdateScenarioRule';
+import {
+    defaultScenarioRuleValues,
+    ScenarioRuleFormValues,
+    useScenarioRuleFormState,
+} from '../../../hooks/useScenarioRuleFormState';
+import { ScenarioRule } from '../../../types/scenarioRule';
+import { ScenarioRuleForm } from './ScenarioRuleForm';
+import { ScenarioRuleFormHeader } from './ScenarioRuleFormHeader';
+
+type Props = {
+    scenarioId: number;
+    rule?: ScenarioRule;
+    onClose: () => void;
+    onBlur?: (values: Partial<ScenarioRuleFormValues>) => void;
+};
+
+export const ScenarioRuleFormWrapper: FC<Props> = ({
+    scenarioId,
+    rule,
+    onClose,
+    onBlur,
+}) => {
+    const { formatMessage } = useSafeIntl();
+
+    const { mutate: createUpdateScenarioRule, isLoading: isSubmittingRule } =
+        useCreateUpdateScenarioRule(scenarioId);
+
+    const title = useMemo(() => {
+        if (rule) {
+            return formatMessage(MESSAGES.editScenarioRule);
+        }
+        return formatMessage(MESSAGES.createScenarioRule);
+    }, [formatMessage, rule]);
+
+    const initialValues: ScenarioRuleFormValues | undefined = useMemo(
+        () =>
+            rule
+                ? {
+                      id: rule.id,
+                      scenario: rule.scenario,
+                      name: rule.name,
+                      color: rule.color,
+                      intervention_properties: rule.intervention_properties,
+                      matching_criteria: rule.matching_criteria,
+                      org_units_excluded: rule.org_units_excluded,
+                      org_units_included: rule.org_units_included,
+                  }
+                : { ...defaultScenarioRuleValues, scenario: scenarioId },
+        [rule, scenarioId],
+    );
+
+    const onSubmit = useCallback(
+        (values: Partial<ScenarioRuleFormValues>) => {
+            createUpdateScenarioRule(values, {
+                onSuccess: () => {
+                    onClose();
+                },
+            });
+        },
+        [createUpdateScenarioRule, onClose],
+    );
+
+    const formik = useScenarioRuleFormState({
+        onSubmit,
+        initialValues,
+        editMode: Boolean(rule),
+    });
+
+    const handleBlur = useCallback(() => {
+        if (onBlur) {
+            onBlur(formik.values);
+        }
+        // We are not checking the field on blur as we only trigger this for fields which should trigger the preview call.
+    }, [onBlur, formik.values]);
+
+    return (
+        <CardStyled
+            header={
+                <ScenarioRuleFormHeader
+                    title={title}
+                    onCancel={onClose}
+                    onSubmit={formik.handleSubmit}
+                    disabled={isSubmittingRule}
+                />
+            }
+            isLoading={isSubmittingRule}
+        >
+            <ExtendedFormikProvider formik={formik} onBlur={handleBlur}>
+                <ScenarioRuleForm />
+            </ExtendedFormikProvider>
+        </CardStyled>
+    );
+};
---
+++ js/src/domains/planningV2/components/scenarioRule/scenarioRuleList/ScenarioRuleLine.tsx
@@ -0,0 +1,113 @@
+import React, { FC, useMemo } from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import { Box, Grid, Typography } from '@mui/material';
+import { useSafeIntl } from 'bluesquare-components';
+import { EditIconButton } from 'Iaso/components/Buttons/EditIconButton';
+import { DeleteModal } from 'Iaso/components/DeleteRestoreModals/DeleteModal';
+import { SxStyles } from 'Iaso/types/general';
+import { noOp } from 'Iaso/utils';
+import { MESSAGES } from '../../../../messages';
+import { usePlanningContext } from '../../../contexts/PlanningContext';
+import { useDeleteScenarioRule } from '../../../hooks/useDeleteScenarioRule';
+import { ScenarioRule } from '../../../types/scenarioRule';
+
+const styles: SxStyles = {
+    colorBox: {
+        width: 16,
+        height: 16,
+        mr: 2,
+        borderRadius: '5px',
+    },
+    rulesText: {
+        ml: 4,
+        display: '-webkit-box',
+        WebkitLineClamp: 2,
+        WebkitBoxOrient: 'vertical',
+        overflow: 'hidden',
+        textWrap: 'balance',
+    },
+};
+
+type Props = {
+    scenarioId: number;
+    rule: ScenarioRule;
+    onEdit: (rule: ScenarioRule) => void;
+};
+
+export const ScenarioRuleLine: FC<Props> = ({ scenarioId, rule, onEdit }) => {
+    const { formatMessage } = useSafeIntl();
+    const { mutateAsync: deleteScenarioRule } =
+        useDeleteScenarioRule(scenarioId);
+
+    const { metricTypeCategories, canEditScenario } = usePlanningContext();
+
+    const metricTypes = useMemo(
+        () => metricTypeCategories.flatMap(category => category.items),
+        [metricTypeCategories],
+    );
+    const metricTypeNames = useMemo(
+        () =>
+            metricTypes.reduce(
+                (acc, mt) => acc.set(mt.id, mt.name),
+                new Map<number, string>(),
+            ),
+        [metricTypes],
+    );
+
+    const metricTypeCriterionLabel = useMemo(() => {
+        return rule.matching_criteria
+            ?.map(ip => {
+                const metricName =
+                    ip.metric_type && metricTypeNames.get(ip.metric_type)
+                        ? metricTypeNames.get(ip.metric_type)
+                        : ip.metric_type;
+                return `${metricName} ${ip.operator} ${ip.value ?? ip.string_value}`;
+            })
+            .join(', ');
+    }, [rule, metricTypeNames]);
+
+    return (
+        <React.Fragment key={rule.id}>
+            <Grid container direction="row" alignItems="center" mb={1}>
+                <Box
+                    sx={{
+                        ...styles.colorBox,
+                        bgcolor: rule.color,
+                    }}
+                />
+                <Typography
+                    variant="body1"
+                    fontWeight="medium"
+                    sx={{ flexGrow: 1 }}
+                >
+                    {rule.name}
+                </Typography>
+                {canEditScenario && (
+                    <>
+                        <EditIconButton
+                            onClick={() => onEdit(rule)}
+                            overrideIcon={EditIcon}
+                        />
+                        <DeleteModal
+                            type="icon"
+                            onConfirm={() => deleteScenarioRule(rule.id)}
+                            onCancel={noOp}
+                            titleMessage={MESSAGES.deleteScenarioRule}
+                            iconProps={{}}
+                            backdropClick={true}
+                        >
+                            {formatMessage(MESSAGES.deleteScenarioRuleConfirmMessage)}
+                        </DeleteModal>
+                    </>
+                )}
+            </Grid>
+            <Typography
+                variant="body2"
+                color="textSecondary"
+                sx={styles.rulesText}
+            >
+                {metricTypeCriterionLabel}
+            </Typography>
+        </React.Fragment>
+    );
+};
