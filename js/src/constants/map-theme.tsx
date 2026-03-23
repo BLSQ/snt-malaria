6b5ed5335becc4123002960994d57028f5dfb76b
Bewi <benjamin.wilfart@gmail.com>
2026-03-23 14:30:24 +0100

    Restore map layout

--- js/src/constants/map-theme.tsx
+++ js/src/constants/map-theme.tsx
@@ -1,11 +1,13 @@
+import { theme } from 'bluesquare-components';
+
 export const mapTheme = {
     backgroundColor: '#CFD8DC',
-    borderColor: '#9fa3afcf',
-    shapeColor: '#d0d5e5',
-    shapeWeight: 2,
+    borderColor: '#FFFFFF',
+    shapeColor: '#9fa3af',
+    shapeWeight: 1,
     selectedShapeColor: '#1294FB',
     selectedShapeWeight: 3,
-    activeShapeColor: '#9fa3af51',
-    activeShapeWeight: 8,
-    fillOpacity: 0.75,
+    activeShapeColor: theme.palette.secondary.main,
+    activeShapeWeight: 4,
+    fillOpacity: 0.3,
 };
