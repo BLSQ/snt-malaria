9a8c4944927989d68519c1e5e20027a69cbf8898
Bewi <benjamin.wilfart@gmail.com>
2026-07-24 10:02:11 +0200

    refactor(CompositeLayerChatBot): Use direct react-query mutation for AI messages

--- js/src/domains/compositeLayerEditor/compositeLayerChatBot/useSendCompositeLayerAIMessage.ts
+++ js/src/domains/compositeLayerEditor/compositeLayerChatBot/useSendCompositeLayerAIMessage.ts
@@ -1,15 +1,13 @@
+import { useMutation } from 'react-query';
 import { postRequest } from 'Iaso/libs/Api';
-import { useSnackMutation } from 'Iaso/libs/apiHooks';
 import { CompositeLayerAIRequest, CompositeLayerAIResponse } from './types';
 
 export const useSendCompositeLayerAIMessage = () => {
-    return useSnackMutation<
+    return useMutation<
         CompositeLayerAIResponse,
         Error,
         CompositeLayerAIRequest
-    >({
-        mutationFn: (data: CompositeLayerAIRequest) =>
-            postRequest('/api/snt_malaria/composite_layer_ai/', data),
-        showSuccessSnackBar: false,
-    });
+    >((data: CompositeLayerAIRequest) =>
+        postRequest('/api/snt_malaria/composite_layer_ai/', data),
+    );
 };
