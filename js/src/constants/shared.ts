944ec0581beba7c0db47029a5bd073a4e58e1221
Bewi <benjamin.wilfart@gmail.com>
2026-08-03 14:56:01 +0200

    Feat(UI): Introduce shared constants for data layer years

--- js/src/constants/shared.ts
+++ js/src/constants/shared.ts
@@ -2,7 +2,7 @@ export const currentYear = new Date().getFullYear();
 export const DataLayerYearOptions = Array.from(
     { length: currentYear - 2000 + 1 },
     (_, i) => {
-        const year = currentYear - i;
+        const year = 2000 + i;
         return { label: year.toString(), value: year };
     },
 );
